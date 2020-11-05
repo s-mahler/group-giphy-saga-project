a111de891d7b568d99e478d18b8e915bb66c41f2
RachelSmith500 <rachelsmith9721@gmail.com>
2020-11-05 14:39:30 -0600

    routes added in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Giphy Search!</h1>
-      </div>
+      <Router>
+          <div>
+            <h1>Giphy Search!</h1>
+          </div>
+          <Route exact path= "/search" component ={Search}/>
+          <Route path="/favorites" component ={Favorite}/>
+      </Router>
     );
   }
   
