9573cee69a57a6f3afc311f61f1e0279e3cf4e58
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-11-05 15:43:17 -0600

    created components for Search and Favorite

---
+++ src/components/Search/Search.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import {HashRouter as Router, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Search extends Component {
+
+    render() {
+        return (
+    
+              <div>
+                  <p>Search</p>
+              </div>
+
+        );
+    }
+
+}
+
+
+
+export default connect()(Search);
