f417eb4141efcd3498dfcb864698fff0e0aa598d
Sam Mahler <mahler.sam@gmail.com>
2020-11-06 10:57:20 -0600

    variable search working

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -5,10 +5,8 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  search = req.body;
-  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.KEY_GIPHY}&q=${search}&limit=25&offset=0&rating=g&lang=en`)
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.KEY_GIPHY}&q=${req.query.search}&limit=25&offset=0&rating=g&lang=en`)
   .then((response) => {
-      console.log(response.data);
       res.send(response.data);
   }).catch((error) => {
       alert(error);
